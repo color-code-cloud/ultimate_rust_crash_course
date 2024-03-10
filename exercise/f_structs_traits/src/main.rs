802560e655206f9707efe950cea697f52dbabc48
ColorCode <158784827+color-code-cloud@users.noreply.github.com>
2024-03-10 21:13:19 +0000

    update from iPad/Code Sandbox

--- exercise/f_structs_traits/src/main.rs
+++ exercise/f_structs_traits/src/main.rs
@@ -1,58 +1,42 @@
-// 1. Define a trait named `Bite`
-//
-// Define a single required method, `fn bite(self: &mut Self)`.  We will call this method when we
-// want to bite something.  Once this trait is defined, you should be able to run the program with
-// `cargo run` without any errors.
-//
-//  trait Bite...
+// Define a trait named Sound
+trait Sound {
+    // A method indicating the sound the object makes
+    fn make_sound(&self);
+}
 
+// Define a struct Dog
+struct Dog {
+    name: String,
+}
 
-// 2. Now create a struct named Grapes with a field that tracks how many grapes are left.  If you
-// need a hint, look at how it was done for Carrot at the bottom of this file (you should probably
-// use a different field, though).
-//
-// #[derive(Debug)] // include this line right before your struct definition
-// struct Grapes...
+// Implement the Sound trait for Dog
+impl Sound for Dog {
+    fn make_sound(&self) {
+        println!("{} says woof!", self.name);
+    }
+}
 
+// Define a struct Car
+struct Car {
+    model: String,
+    year: u32,
+}
 
-// 3. Implement Bite for Grapes.  When you bite a Grapes, subtract 1 from how many grapes are left.
-// If you need a hint, look at how it was done for Carrot at the bottom of this file.
-//
-// impl Bite for...
-
+// Implement the Sound trait for Car
+impl Sound for Car {
+    fn make_sound(&self) {
+        println!("The {} car makes a vroom vroom sound.", self.model);
+    }
+}
 
 fn main() {
-    // Once you finish #1 above, this part should work.
-    let mut carrot = Carrot { percent_left: 100.0 };
-    carrot.bite();
-    println!("I take a bite: {:?}", carrot);
+    // Create instances of Dog and Car
+    let my_dog = Dog { name: String::from("Buddy") };
+    let my_car = Car { model: String::from("Toyota Camry"), year: 2022 };
+{.}
+    // Call the make_sound method for Dog
+    my_dog.make_sound();
 
-    // 4. Uncomment and adjust the code below to match how you defined your
-    // Grapes struct.
-    //
-    //let mut grapes = Grapes { amount_left: 100 };
-    //grapes.bite();
-    //println!("Eat a grape: {:?}", grapes);
-
-    // Challenge: Uncomment the code below. Create a generic `bunny_nibbles`
-    // function that:
-    // - takes a mutable reference to any type that implements Bite
-    // - calls `.bite()` several times
-    // Hint: Define the generic type between the function name and open paren:
-    //       fn function_name<T: Bite>(...)
-    //
-    //bunny_nibbles(&mut carrot);
-    //println!("Bunny nibbles for awhile: {:?}", carrot);
-}
-
-#[derive(Debug)] // This enables using the debugging format string "{:?}"
-struct Carrot {
-    percent_left: f32,
-}
-
-impl Bite for Carrot {
-    fn bite(self: &mut Self) {
-        // Eat 20% of the remaining carrot. It may take awhile to eat it all...
-        self.percent_left *= 0.8;
-    }
+    // Call the make_sound method for Car
+    my_car.make_sound();
 }
